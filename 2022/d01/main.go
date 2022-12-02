77ba2b95d96fd4f93a20541b72c57744d76221c5
Quentin Selle <quentin@selle.dev>
2022-12-02 10:19:15 +0100

    oops i forgot the second part

--- 2022/d01/main.go
+++ 2022/d01/main.go
@@ -2,47 +2,39 @@ package main
 
 import (
 	"bufio"
+	"flag"
 	"fmt"
 	"os"
+	"sort"
 	"strconv"
 )
 
-var (
-	inputEnv        = "INPUT"
-	defaultFilename = "input.txt"
-)
+type Elf struct {
+	Number   uint64
+	Calories uint64
+}
 
 func main() {
-	filename := os.Getenv(inputEnv)
-	if filename == "" {
-		filename = defaultFilename
-	}
+	input := flag.String("input", "input.txt", "input to process")
+	limit := flag.Int("limit", 1, "elf limit to print (by descending order)")
+	flag.Parse()
 
-	f, err := os.Open(filename)
+	f, err := os.Open(*input)
 	if err != nil {
 		panic(err)
 	}
 	defer f.Close()
 
 	s := bufio.NewScanner(f)
+	var currentCalories, elfNumber uint64 = 0, 0
+	var elfes []Elf
 
-	var lines []string
 	for s.Scan() {
-		lines = append(lines, s.Text())
-	}
-	if s.Err() != nil {
-		panic(err)
-	}
-
-	var currentCalories, maxCalories uint64 = 0, 0
-	var maxCaloriesElfID = 0
-	for elfID, line := range lines {
+		line := s.Text()
 		if line == "" {
-			if currentCalories > maxCalories {
-				maxCalories = currentCalories
-				maxCaloriesElfID = elfID
-			}
+			elfes = append(elfes, Elf{elfNumber + 1, currentCalories})
 			currentCalories = 0
+			elfNumber += 1
 			continue
 		}
 		foodCalories, err := strconv.ParseUint(line, 10, 64)
@@ -51,5 +43,20 @@ func main() {
 		}
 		currentCalories += foodCalories
 	}
-	fmt.Printf("The Elf carrying the most calories is the number %d with: %d calories.\n", maxCaloriesElfID, maxCalories)
+	if s.Err() != nil {
+		panic(err)
+	}
+
+	sort.SliceStable(elfes, func(i, j int) bool {
+		return elfes[i].Calories > elfes[j].Calories
+	})
+	var total uint64
+	for num, elf := range elfes {
+		if num == *limit {
+			break
+		}
+		fmt.Printf("I'm %d and I'm carrying %d\n", elf.Number, elf.Calories)
+		total += elf.Calories
+	}
+	fmt.Printf("%d calories carrying by the top %d elf(es)\n", total, *limit)
 }
