2ded9c57e48c631131ecc5a00a55125c1e39efd6
Quentin Selle <quentin@selle.dev>
2022-12-04 13:36:55 +0100

    second day with the second part

--- 2022/d02/main.go
+++ 2022/d02/main.go
@@ -38,8 +38,39 @@ var battle = map[[2]int]int{
 	{scissors, scissors}: draw,
 }
 
+func scoreCalcultationPart1(line string) int {
+	game := strings.Split(line, " ")
+	opponent, me := unify[game[0]], unify[game[1]]
+	return battle[[2]int{opponent, me}] + me
+}
+
+var battleStrategy = map[[2]int]int{
+	{rock, win}:      paper,
+	{rock, draw}:     rock,
+	{rock, lose}:     scissors,
+	{paper, win}:     scissors,
+	{paper, draw}:    paper,
+	{paper, lose}:    rock,
+	{scissors, win}:  rock,
+	{scissors, draw}: scissors,
+	{scissors, lose}: paper,
+}
+
+var strategyToDo = map[string]int{
+	"X": lose,
+	"Y": draw,
+	"Z": win,
+}
+
+func scoreCalcultationPart2(line string) int {
+	game := strings.Split(line, " ")
+	opponent, needTo := unify[game[0]], strategyToDo[game[1]]
+	return battleStrategy[[2]int{opponent, needTo}] + needTo
+}
+
 func main() {
 	input := flag.String("input", "input.txt", "aoc input to process.")
+	part := flag.Int("part", 1, "aoc exercice part (1 or 2).")
 	flag.Parse()
 
 	f, err := os.Open(*input)
@@ -51,10 +82,11 @@ func main() {
 	myScore := 0
 	s := bufio.NewScanner(f)
 	for s.Scan() {
-		line := s.Text()
-		game := strings.Split(line, " ")
-		opponent, me := unify[game[0]], unify[game[1]]
-		myScore += battle[[2]int{opponent, me}] + me
+		if *part == 1 {
+			myScore += scoreCalcultationPart1(s.Text())
+		} else {
+			myScore += scoreCalcultationPart2(s.Text())
+		}
 	}
 	if s.Err() != nil {
 		panic(err)
