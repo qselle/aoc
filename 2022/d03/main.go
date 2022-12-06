c241ea88dfa79fb23d3eb7b5d776670bb7001f3d
Quentin Selle <quentin@selle.dev>
2022-12-06 21:14:15 +0100

    second part of day 3

--- 2022/d03/main.go
+++ 2022/d03/main.go
@@ -17,26 +17,52 @@ func getItemPriority(c rune) int {
 
 func main() {
 	input := flag.String("input", "input.txt", "aoc input to process")
-	// part := flag.Int("part", 1, "aoc part (can be 1 or 2)")
+	part := flag.Int("part", 1, "aoc part (can be 1 or 2)")
 	flag.Parse()
 
 	f, err := os.Open(*input)
 	if err != nil {
 		panic(err)
 	}
-	sumDoubleItemPriority := 0
-	s := bufio.NewScanner(f)
-	for s.Scan() {
-		line := s.Text()
-		currentDoubleItemPriority := 0
-		start, end := line[:len(line)/2], line[len(line)/2:]
-		for _, item := range start {
-			if strings.ContainsRune(end, item) {
-				currentDoubleItemPriority = getItemPriority(item)
-				break
+	if *part == 1 {
+		sumDoubleItemPriority := 0
+		s := bufio.NewScanner(f)
+		for s.Scan() {
+			line := s.Text()
+			currentDoubleItemPriority := 0
+			start, end := line[:len(line)/2], line[len(line)/2:]
+			for _, item := range start {
+				if strings.ContainsRune(end, item) {
+					currentDoubleItemPriority = getItemPriority(item)
+					break
+				}
+			}
+			sumDoubleItemPriority += currentDoubleItemPriority
+		}
+		fmt.Println(sumDoubleItemPriority)
+	} else if *part == 2 {
+		index := 0
+		sum := 0
+		s := bufio.NewScanner(f)
+		var group []string
+		for s.Scan() {
+			index++
+			line := s.Text()
+			group = append(group, line)
+			if index%3 == 0 {
+				for _, item := range group[0] {
+					if strings.ContainsRune(group[1], item) {
+						if strings.ContainsRune(group[2], item) {
+							sum += getItemPriority(item)
+							break
+						}
+					}
+				}
+				group = nil
 			}
 		}
-		sumDoubleItemPriority += currentDoubleItemPriority
+		fmt.Println(sum)
+	} else {
+		fmt.Println("wrong part dumbass")
 	}
-	fmt.Println(sumDoubleItemPriority)
 }
